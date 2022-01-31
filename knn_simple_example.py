9ad58ec36a23029872896d8929247f9e1f4288d0
Gaurav-AL <reverse454@gmail.com>
2022-01-31 23:49:44 +0530

    Done

--- knn_simple_example.py
+++ knn_simple_example.py
@@ -13,6 +13,17 @@ So, simply we will find euclidean distance of Query with every Dataset (Math,Com
 Here 1,1,2 will be the nearest euclidean distance with Query(math=6,comp=8) and result will be pass
 
 One can simply try this Datatset with different value of k and then can find out why Cross-validation is a required.
+
+Ouput for validating for coodinates math =6 and comp = 8
+k = 1, pass
+k = 2, pass
+k = 3, pass
+k = 4, fail
+k = 5, fail
+k = 6, fail
+
+Since we know that for math =6 and comp = 8 ,  belongs to pass class but after exceeding value of k more than 3
+it is giving wrong result, so we can say that for k <= 3 is right for this dataset.
 '''
 from itertools import count
 import math
@@ -24,7 +35,7 @@ dataset = [[4,6,7,5,8],[3,7,8,5,8],['fail','pass','pass','fail','pass']]
 query = [int(x) for x in input("Enter Query :").split(" ")]
 k = int(input("Enter Value of K :"))
 if((len(dataset[0]) != len(dataset[1])) or (len(dataset[1]) != len(dataset[2]))):
-    print("Fuck OFF with your dataset")
+    print("You have wrong Datatset")
 lookup = {}
 for i in range(len(dataset[0])):
     lookup[i] = euclidean_distance(dataset[0][i],dataset[1][i],query[0],query[1])
